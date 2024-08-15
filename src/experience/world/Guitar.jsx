e245774a387c8d90d0148f35616cde95f35485cd
LeWilSy <b.tighidet0@gmail.com>
2024-08-16 00:45:45 +0200

    update guiatr animation

--- src/experience/world/Guitar.jsx
+++ src/experience/world/Guitar.jsx
@@ -14,12 +14,12 @@ export default function Guitar({ onGuitarLoaded }) {
   useEffect(() => {
     if (guitar.current) {
       gsap.to(guitar.current.position, {
-        z: 0.5,
+        z: 0.6,
         x: 0.1,
         ease: "power1.inOut",
         scrollTrigger: {
           trigger: "#legacy",
-          start: "bottom 40%",
+          start: "bottom 60%",
           endTrigger: "#performances",
           end: "top bottom",
           scrub: true,
