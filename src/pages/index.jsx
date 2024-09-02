0933ac1333ab0b4ea95431f73ee0adc8aa830cec
Billel T <148473122+BillelT@users.noreply.github.com>
2024-09-03 01:52:24 +0200

    try: debug on ios song doesnt play after interaction

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -86,14 +86,16 @@ export default function Index({
     if (!isStarted) return;
 
     if (song.current) {
+      song.current.addEventListener("canplaythrough", () => {
+        song.current.play().catch((error) => {
+          console.error("Playback prevented:", error);
+        });
+      });
+
       song.current.loop = true;
       song.current.volume = 1;
       song.current.play();
     }
-
-    song.current.loop = true;
-    song.current.volume = 1;
-    song.current.play();
 
     song.current.load();
 
