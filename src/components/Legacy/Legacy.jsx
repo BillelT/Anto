46f0e533dbc549859bfd6e93c85a4e824f586177
LeWilSy <b.tighidet0@gmail.com>
2025-03-02 12:36:47 +0100

    add space in leagcy text

--- src/components/Legacy/Legacy.jsx
+++ src/components/Legacy/Legacy.jsx
@@ -6,7 +6,7 @@ export default function Legacy() {
           <h2 className="pb-16">Arts & Traditions</h2>
           <p className="body pb-8">
             Nos instruments sont réalisés selon l'art et la tradition hérités
-            <span className="underline">d'Antonio de Torres.</span>
+            &nbsp;<span className="underline">d'Antonio de Torres.</span>
           </p>
           <p className="body">
             Chaque modèle est façonné à la main par nos luthiers, qui y
